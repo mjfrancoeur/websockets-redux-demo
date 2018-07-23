41e8b10b4ebace75ceb80e6194d0dea70cee1eaf
Michael Francoeur <michael.francoeur@ibm.com@michaels-mbp-2.dfw.ibm.com>
2018-07-22 20:20:09 -0400

    Remove unneeded code and components

--- java/src/main/java/com/greglturnquist/payroll/WebSocketConfiguration.java
+++ java/src/main/java/com/greglturnquist/payroll/WebSocketConfiguration.java
@@ -33,7 +33,6 @@ public class WebSocketConfiguration extends AbstractWebSocketMessageBrokerConfig
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/payroll").setAllowedOrigins("*").withSockJS();
 		registry.addEndpoint("/threats").setAllowedOrigins("*").withSockJS();
 	}
 
